4200b9d3a548eb16eba49bf62eb81948a66781ad
Mauro Riva <lemariva@gmail.com>
2020-11-01 22:48:48 +0100

    added more needed libraries and cleaned up

--- photoservice/app/restapi.py
+++ photoservice/app/restapi.py
@@ -20,7 +20,6 @@ import sys
 import argparse
 import struct
 import cv2
-from pydng.core import RPICAM2DNG
 import exifread
 import time
 import subprocess
@@ -47,6 +46,7 @@ hdr_b = np.array([30, 25, 20, 15, 13, 10, 8, 6, 5, 4, 3.2, 2.5, 2, 1.6, 1.3, 1, 
                   0.5, 0.4, 0.3, 1/4, 1/5, 1/6, 1/8, 1/10, 1/20, 1/25, 1/30, 1/40, 1/50, 1/60,
                   1/80, 1/100, 1/125, 1/160, 1/200, 1/250, 1/320, 1/400, 1/500, 1/640, 1/800,
                   1/1000, 1/1250, 1/1600, 1/2000, 1/2500, 1/3200, 1/4000, 1/5000, 1/6400, 1/8000])
+
 # celery --app=restapi.celery worker -l INFO
 
 def find_nearest(array, value):
