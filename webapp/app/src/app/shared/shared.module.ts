1bd7d2b65b46e56a503f63e4a7116ebf5b2b83ce
Mauro Riva <lemariva@gmail.com>
2020-11-08 12:21:03 +0100

    changed style & template of the gallery

--- webapp/app/src/app/shared/shared.module.ts
+++ webapp/app/src/app/shared/shared.module.ts
@@ -1,32 +1,14 @@
  
-
-/*
-  Copyright (C) 2020 Mauro Riva
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { HttpModule } from "@angular/http";
-import {CdkTableModule} from '@angular/cdk/table';
-import {FlexLayoutModule} from "@angular/flex-layout";
-import {MainLayoutModule} from "./layouts/main-layout/main-layout.module";
+import { CdkTableModule } from '@angular/cdk/table';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MainLayoutModule } from "./layouts/main-layout/main-layout.module";
 import { GalleryModule } from  'ng-gallery';
-
+import { GALLERY_CONFIG } from 'ng-gallery';
 
 @NgModule({
     imports: [
@@ -58,6 +40,17 @@ import { GalleryModule } from  'ng-gallery';
     ],
     declarations: [],
     providers: [
+        {
+            provide: GALLERY_CONFIG,
+            useValue: {
+              dots: true,
+              imageSize: 'contain',
+              thumbMode: 'strict',
+              loadingStrategy: 'lazy',
+              gestures: 'true',
+              autoPlay: 'true'
+            }
+        }
     ],
     entryComponents: []
 })
